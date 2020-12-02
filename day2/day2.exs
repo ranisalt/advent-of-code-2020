2366be43e08d678f58eb5f406e69793dbe1e7bf8
Ranieri Althoff <ranisalt@gmail.com>
2020-12-02 14:35:26 +0100

    Implement day 2, part 2

--- day2/day2.exs
+++ day2/day2.exs
@@ -2,16 +2,30 @@ defmodule Day2 do
   def main() do
     re = ~r/(?<min>\d+)-(?<max>\d+) (?<char>\w): (?<pwd>\w+)/
 
-    IO.stream(:stdio, :line)
-    |> Enum.count(fn x ->
-      captures = Regex.named_captures(re, x)
+    rules =
+      IO.stream(:stdio, :line)
+      |> Enum.map(fn x ->
+        captures = Regex.named_captures(re, x)
+        min = String.to_integer(captures["min"])
+        max = String.to_integer(captures["max"])
 
+        {min, max, captures["char"], captures["pwd"]}
+      end)
+
+    rules
+    |> Enum.count(fn {min, max, char, pwd} ->
       count =
-        captures["pwd"]
+        pwd
         |> String.graphemes()
-        |> Enum.count(&(&1 == captures["char"]))
+        |> Enum.count(&(&1 == char))
 
-      String.to_integer(captures["min"]) <= count && count <= String.to_integer(captures["max"])
+      min <= count && count <= max
+    end)
+    |> IO.puts()
+
+    rules
+    |> Enum.count(fn {min, max, char, pwd} ->
+      String.at(pwd, min - 1) == char != (String.at(pwd, max - 1) == char)
     end)
     |> IO.puts()
   end
