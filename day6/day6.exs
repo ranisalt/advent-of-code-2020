840c16e928ea3f0ad70a5d0e4fa4745e690fe879
Ranieri Althoff <ranisalt@gmail.com>
2020-12-06 11:18:26 +0100

    Implement day 6, parts 1 and 2

---
+++ day6/day6.exs
@@ -0,0 +1,28 @@
+defmodule Day6 do
+  defp mapset_operate(data, reducer) do
+    Enum.map(data, fn answers ->
+      Enum.reduce(answers, reducer)
+      |> Enum.count()
+    end)
+    |> Enum.reduce(fn x, acc -> acc + x end)
+    |> IO.puts()
+  end
+
+  def main() do
+    data =
+      IO.read(:stdio, :all)
+      |> String.split(~r{\n\n})
+      |> Enum.map(fn answers ->
+        String.split(answers)
+        |> Enum.map(fn x ->
+          String.graphemes(x)
+          |> MapSet.new()
+        end)
+      end)
+
+    mapset_operate(data, fn x, acc -> MapSet.union(x, acc) end)
+    mapset_operate(data, fn x, acc -> MapSet.intersection(x, acc) end)
+  end
+end
+
+Day6.main()
