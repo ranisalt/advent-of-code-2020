7b97d64a078d0af5632e509587319bfac8f77711
Ranieri Althoff <ranisalt@gmail.com>
2020-12-09 03:40:20 +0100

    Implement day 8, part 1

---
+++ day8/day8.exs
@@ -0,0 +1,38 @@
+defmodule Day8 do
+  defp next_state(instructions, pc, acc, history) do
+    next_history = MapSet.put(history, pc)
+    {inst, imm} = Enum.at(instructions, pc)
+
+    case inst do
+      "acc" -> {pc + 1, acc + imm, next_history}
+      "jmp" -> {pc + imm, acc, next_history}
+      "nop" -> {pc + 1, acc, next_history}
+    end
+  end
+
+  defp try_exec(instructions, state) do
+    # infinite loop with counter
+    Enum.reduce_while(0..Enum.count(instructions), state, fn _, {pc, acc, history} ->
+      if MapSet.member?(history, pc) do
+        {:halt, acc}
+      else
+        {:cont, next_state(instructions, pc, acc, history)}
+      end
+    end)
+  end
+
+  def main() do
+    instructions =
+      IO.stream(:stdio, :line)
+      |> Enum.map(fn line ->
+        [inst, imm] = Regex.run(~r/(\w+) ([+-]\d+)/, line, capture: :all_but_first)
+
+        {inst, String.to_integer(imm)}
+      end)
+
+    try_exec(instructions, {0, 0, MapSet.new()})
+    |> IO.inspect()
+  end
+end
+
+Day8.main()
