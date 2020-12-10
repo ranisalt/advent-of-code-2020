8fd9c6fd667ba0de17791926522e44d472d03e06
Ranieri Althoff <ranisalt@gmail.com>
2020-12-10 21:13:00 +0100

    Implement day 7, part 2

--- day7/day7.exs
+++ day7/day7.exs
@@ -1,31 +1,47 @@
 defmodule Day7 do
-  defp dfs(rules, acc, node) do
+  defp contained_bags(rules, node) do
+    Map.get(rules, node, MapSet.new())
+    |> Stream.map(fn {color, amount} ->
+      amount + amount * contained_bags(rules, color)
+    end)
+    |> Enum.sum()
+  end
+
+  defp outermost_color(rules, acc, node) do
     open =
       Map.get(rules, node, MapSet.new())
       |> MapSet.difference(acc)
 
     Enum.reduce(open, open, fn x, acc ->
-      dfs(rules, acc, x)
+      outermost_color(rules, acc, x)
       |> MapSet.union(acc)
     end)
     |> MapSet.union(acc)
   end
 
   def main() do
-    IO.stream(:stdio, :line)
-    |> Enum.reduce(Map.new(), fn x, acc ->
-      [_, container, containees] = Regex.run(~r/^(.*) bags contain (.*)\.$/, x)
+    contains =
+      IO.stream(:stdio, :line)
+      |> Enum.reduce(Map.new(), fn x, acc ->
+        [container, containees] =
+          Regex.run(~r/^(.*) bags contain (.*)\.$/, x, capture: :all_but_first)
 
-      String.split(containees, ", ")
-      |> Enum.reduce(MapSet.new(), fn y, acc ->
-        [_, amount, color] = Regex.run(~r/(no|\d+) (.*) bags?/, y)
+        contained =
+          String.split(containees, ", ")
+          |> Enum.reduce(MapSet.new(), fn y, acc ->
+            [amount, color] = Regex.run(~r/(no|\d+) (.*) bags?/, y, capture: :all_but_first)
 
-        case amount do
-          "no" -> acc
-          _ -> MapSet.put(acc, color)
-        end
+            case amount do
+              "no" -> acc
+              _ -> MapSet.put(acc, {color, String.to_integer(amount)})
+            end
+          end)
+
+        Map.put(acc, container, contained)
       end)
-      |> Enum.reduce(acc, fn y, acc ->
+
+    Enum.reduce(contains, Map.new(), fn {container, colors}, acc ->
+      Enum.reduce(colors, acc, fn {y, _}, acc ->
         new_value =
           Map.get(acc, y, MapSet.new())
           |> MapSet.put(container)
@@ -33,8 +49,11 @@ defmodule Day7 do
         Map.put(acc, y, new_value)
       end)
     end)
-    |> dfs(MapSet.new(), "shiny gold")
+    |> outermost_color(MapSet.new(), "shiny gold")
     |> MapSet.size()
+    |> IO.inspect()
+
+    contained_bags(contains, "shiny gold")
     |> IO.inspect()
   end
 end
