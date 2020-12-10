46378e458a142f6093a99e1023670d72fd2fba58
Ranieri Althoff <ranisalt@gmail.com>
2020-12-10 18:59:26 +0100

    Implement day 9, parts 1 and 2

---
+++ day9/day9.exs
@@ -0,0 +1,45 @@
+defmodule Day9 do
+  def main() do
+    input =
+      IO.stream(:stdio, :line)
+      |> Enum.map(fn x -> String.trim_trailing(x) |> String.to_integer() end)
+
+    limit = 25
+    preamble = Enum.take(input, limit)
+
+    invalid =
+      Enum.drop(input, limit)
+      |> Enum.reduce_while(preamble, fn z, acc ->
+        terms_in_preamble =
+          Enum.any?(acc, fn x ->
+            Enum.any?(acc, fn y ->
+              z == x + y
+            end)
+          end)
+
+        if terms_in_preamble do
+          [_ | tail] = acc
+          {:cont, tail ++ [z]}
+        else
+          {:halt, z}
+        end
+      end)
+      |> IO.inspect()
+
+    last = Enum.count(input) - 1
+
+    Enum.find_value(0..last, fn i ->
+      Enum.find_value(i..last, fn j ->
+        slice = Enum.slice(input, i..j)
+
+        if Enum.sum(slice) == invalid do
+          sorted = Enum.sort(slice)
+          List.first(sorted) + List.last(sorted)
+        end
+      end)
+    end)
+    |> IO.inspect()
+  end
+end
+
+Day9.main()
